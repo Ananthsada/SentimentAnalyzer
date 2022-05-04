b6bbdafc1729441465932f9aa77dd3a815050c0a
Sadaananth Anbucheliyan <ananth.anbucheliyan@gmail.com>
2022-05-04 23:02:33 +0800

    Filter stop words

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,36 @@
 #include <iostream>
-#include "spacy/spacy"
-#include <fstream>
 #include <string>
+
+#include "spacy/spacy"
 #include "csv.hpp"
 
-const std::string dataSet = "/mnt/c/Study/NLP/FreeCodeCamp/IMDB_Dataset.csv";
+const std::string dataSet = "/mnt/c/Study/NLP/FreeCodeCamp/IMDB_Dataset_Stripped.csv";
 int main()
 {
     Spacy::Spacy spacy;
     auto nlp = spacy.load("en_core_web_sm");
 
     csv::CSVReader reader(dataSet);
+    auto firstRow = reader.begin();
+    auto firstField = firstRow->begin();
+    std::string example = firstField->get();
 
-    auto columnNames = reader.get_col_names();
+    auto Doc = nlp.parse(example);
 
-    for(auto each : columnNames)
+    std::vector<Spacy::Token> _filteredTokens;
+    for (auto& token : Doc.tokens())
     {
-        std::cout << each << "\n";
+        if(!token.is_stop())
+        {
+            _filteredTokens.emplace_back(token);
+        }
     }
+
+    for(auto each : _filteredTokens)
+    {
+        std::cout << each.text() << " ";
+    }
+        
 
     return 0;
 }
